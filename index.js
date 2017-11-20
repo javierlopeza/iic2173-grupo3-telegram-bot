552ceb7803ca82ad4e1232d1467e139bb11b5e17
Vicente Fuenzalida <vjfuenzalida@uc.cl>
2017-11-19 21:21:31 -0300

    Example bot

--- index.js
+++ index.js
@@ -1,42 +1,91 @@
 /**
- * This example demonstrates setting up a webook, and receiving
- * updates in your express app
+ * This example demonstrates using polling.
+ * It also demonstrates how you would process and send messages.
  */
-/* eslint-disable no-console */
 
 require('dotenv').config();
 
-const TOKEN = process.env.TELEGRAM_TOKEN;
-const url = process.env.APP_URL;
-const port = process.env.PORT  || 5000;;
-
+const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-const bodyParser = require('body-parser');
+const request = require('request');
 
-// No need to pass any parameters as we will handle the updates with Express
-const bot = new TelegramBot(TOKEN);
+const options = {
+  polling: true
+};
 
-// This informs the Telegram servers of the new webhook.
-bot.setWebHook(`${url}/bot${TOKEN}`);
+const bot = new TelegramBot(TOKEN, options);
 
-const app = express();
 
-// parse the updates to JSON
-app.use(bodyParser.json());
-
-// We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
+// Matches /photo
+bot.onText(/\/photo/, function onPhotoText(msg) {
+  // From file path
+  const photo = './public/photo.jpg';
+  bot.sendPhoto(msg.chat.id, photo, {
+    caption: "I'm a bot!"
+  });
 });
 
-// Start Express Server
-app.listen(port, () => {
-  console.log(`Express server is listening on ${port}`);
+
+// Matches /audio
+bot.onText(/\/audio/, function onAudioText(msg) {
+  // From HTTP request
+  const audio = './public/song.mp3';
+  bot.sendAudio(msg.chat.id, audio);
 });
 
-// Just to ping!
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
+
+// Matches /love
+bot.onText(/\/love/, function onLoveText(msg) {
+  const opts = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['Yes, you are the bot of my life ❤'],
+        ['No, sorry there is another one...']
+      ]
+    })
+  };
+  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
+});
+
+
+// Matches /echo [whatever]
+bot.onText(/\/echo (.+)/, function onEchoText(msg, match) {
+  const resp = match[1];
+  bot.sendMessage(msg.chat.id, resp);
+});
+
+
+// Matches /editable
+bot.onText(/\/editable/, function onEditableText(msg) {
+  const opts = {
+    reply_markup: {
+      inline_keyboard: [
+        [{
+          text: 'Edit Text',
+          // we shall check for this value when we listen
+          // for "callback_query"
+          callback_data: 'edit'
+        }]
+      ]
+    }
+  };
+  bot.sendMessage(msg.from.id, 'Original Text', opts);
+});
+
+
+// Handle callback queries
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+  const action = callbackQuery.data;
+  const msg = callbackQuery.message;
+  const opts = {
+    chat_id: msg.chat.id,
+    message_id: msg.message_id,
+  };
+  let text;
+  if (action === 'edit') {
+    text = 'Edited Text';
+  }
+
+  bot.editMessageText(text, opts);
 });
